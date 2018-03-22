785b16b0cb83b631188fa27bcc68599e8ba20670
Alexander Schelchere <alexanderschelchere@ool-ad020033.dyn.optonline.net>
2018-03-22 16:02:00 -0400

    setting up basic foundation of app

--- models/User.js
+++ models/User.js
@@ -11,16 +11,20 @@ Users.findAll = () => db.any("SELECT * FROM users ORDER BY id");
 Users.profile = id => db.one("SELECT username FROM users WHERE id = $1", [id]);
 
 // Create your acount
-Users.create = data => db.one(
+Users.create = data =>
+  db.one(
     "INSERT INTO users (name, email, username, password) VALUES ($1, $2, $3, $4) RETURNING id",
     [data.name, data.email, data.username, data.password]
   );
 
 // edit your account
-Users.edit = (data, id) => db.one(
+Users.edit = (data, id) =>
+  db.one(
     "UPDATE users SET name = $1, email = $2, username = $3 WHERE id = $4",
     [data.name, data.email, data.username, id]
   );
 
 // delete account
 Users.delete = id => db.one("DELETE FROM users WHERE id = $1", [id]);
+
+module.exports = Users;
